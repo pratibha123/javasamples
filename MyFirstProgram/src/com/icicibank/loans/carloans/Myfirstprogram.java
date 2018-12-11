b9f58c15d66e1025553439fa2dcb6b78ebbf9fca
pratibha123 <pratibha.juturu@gmail.com>
2018-12-10 19:55:51 -0500

    added my project
    added my project

---
+++ MyFirstProgram/src/com/icicibank/loans/carloans/Myfirstprogram.java
@@ -0,0 +1,10 @@
+package com.icicibank.loans.carloans;
+
+public class Myfirstprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+	}
+
+}
---
+++ MyFirstProgram/src/com/icicibank/loans/homeloans/Mysecondprogram.java
@@ -0,0 +1,10 @@
+package com.icicibank.loans.homeloans;
+
+public class Mysecondprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hi");
+	}
+
+}
